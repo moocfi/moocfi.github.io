2d8ca60ecfdd04ebef73b90dd952b75e6a033555
ljleppan <leo.leppanen@helsinki.fi>
2015-01-22 15:29:02 +0200

    Material menu

--- js/material.js
+++ js/material.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
 	$(".exercise header").each( function () {
 		$(this).next().toggle();
 	})
+
+	var menuButton = $('#week-selector-toggle');
+	menuButton.click(function() {
+		menuButton.toggleClass('active');
+		$('#week-selector').toggleClass('active');
+	});
 });
 
 $(".exercise header").click( function () {
